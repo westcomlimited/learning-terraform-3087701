36c1ee73eba0003664f33bac49894d49d8d50f80
westcomlimited <138551803+westcomlimited@users.noreply.github.com>
2023-07-05 11:19:42 +1000

    fix order

--- main.tf
+++ main.tf
@@ -31,6 +31,19 @@ module "vpc" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id  = module.vpc.public_subnets[0]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"] 
+  
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -82,15 +95,3 @@ module "alb" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog_new"
-
-  vpc_id  = module.vpc.public_subnets[0]
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"] 
-  
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
