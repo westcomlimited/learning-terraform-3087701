1cd20730db94fbddfaa61586619d467a6b9252f9
westcomlimited <138551803+westcomlimited@users.noreply.github.com>
2023-07-05 10:43:28 +1000

    try with 2 subnets

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["ap-southeast-2a", "ap-southeast-2b"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
 
