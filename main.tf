8d9cc2a2757cf0c2c5c2b5d59ce9b2c8b27621a4
westcomlimited <138551803+westcomlimited@users.noreply.github.com>
2023-07-05 10:46:21 +1000

    try first SN

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog_new"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.public_subnets[1]
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] 
   
