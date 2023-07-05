67b581b818f342c58c57e730a922fb02b9c6aa94
westcomlimited <138551803+westcomlimited@users.noreply.github.com>
2023-07-05 11:10:09 +1000

    remove vpc statement in instance block

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  vpc_id                 = module.vpc.id
 
   tags = {
     Name = "Learning Terraform"
