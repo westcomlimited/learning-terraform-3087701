dd65f551878049084d79ef8a4a120662de06f54c
westcomlimited <138551803+westcomlimited@users.noreply.github.com>
2023-08-01 15:17:43 +1000

    fix cidr blocks

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = "${var.environment.network_prefix}0.0.0/16"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["ap-southeast-2a","ap-southeast-2b","ap-southeast-2c"]
-  public_subnets  = ["${var.environment.network_prefix}0.101.0/24", "${var.environment.network_prefix}0.102.0/24", "${var.environment.network_prefix}0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
 #90  enable_nat_gateway = true
 
